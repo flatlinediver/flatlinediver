0c18082bf07fa99154dd50aa0ac9c19c9b8a3a96
flatlinediver <contact@flatlinediver.com>
2022-03-11 07:44:47 +0400

    c

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = {
     return {
       beforeFiles: [
         {
+          "source": "/demos/:demo",
+          "destination": "https://www.:demo.flatlinediver.com",
+        },
+        {
           "source": "/demos/:demo/:resources*",
           "destination": "https://www.:demo.flatlinediver.com/:resources*",
         }
