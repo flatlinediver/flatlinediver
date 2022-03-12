e36dc1a5ce12bf2d41733a53b252692802205b79
flatlinediver <contact@flatlinediver.com>
2022-03-13 02:00:28 +0400

    animate theme toggle

--- src/ui/theme-toggle.tsx
+++ src/ui/theme-toggle.tsx
@@ -1,12 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { motion, useAnimation, useReducedMotion } from 'framer-motion';
 import { useThemeMode } from '@context/theme-mode';
 import { RiSunFill, RiMoonFill } from 'react-icons/ri';
 
 export const ThemeToggle: FC = () => {
   const { mode, toggleMode } = useThemeMode();
+  const shouldReduceMotion = useReducedMotion();
+  const controls = useAnimation();
+  const initial = { rotate: mode === `light` ? 360 : 0 };
+  const ifShould = (obj = {}) => (shouldReduceMotion ? { type: false } : obj);
+
+  useEffect(() => {
+    controls.start({
+      opacity: 1,
+      rotate: mode === `light` ? 360 : 0,
+      transition: ifShould({ type: 'spring', stiffness: 100 }),
+    });
+  }, [mode]);
 
   return (
-    <button
+    <motion.button
+      initial={initial}
+      animate={controls}
       onClick={() => toggleMode()}
       aria-label="Dark mode"
       role="switch"
@@ -14,6 +29,6 @@ export const ThemeToggle: FC = () => {
     >
       <b className="visually-hidden">{mode === `light` ? `dark mode` : `Light mode`}</b>
       {mode === `light` ? <RiSunFill /> : <RiMoonFill />}
-    </button>
+    </motion.button>
   );
 };
