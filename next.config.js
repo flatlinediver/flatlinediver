c177fb373270f27d1c3b17483e16e8de4a06d224
flatlinediver <contact@flatlinediver.com>
2022-03-17 14:15:23 +0400

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,33 @@
 // next.config.js
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        "source": "/:path*",
+        "headers": [
+          {
+            "key": "home",
+            "value": "true"
+          }
+        ]
+      }
+    ]
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          "source": "/lab/:lab",
+          "destination": "https://www.:lab.flatlinediver.com/",
+          "basePath": false
+        },
+        {
+          "source": "/lab/:lab/:resources*",
+          "destination": "https://www.:lab.flatlinediver.com/:resources*",
+          "basePath": false
+        }
+      ]
+    }
+  },
 }
