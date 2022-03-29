6b5fa872649c4b794075a302dc70e29767ca3d54
flatlinediver <contact@flatlinediver.com>
2022-03-29 15:27:25 +0400

    removed rewrites

--- next.config.js
+++ next.config.js
@@ -1,33 +1,4 @@
 // next.config.js
 module.exports = {
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        "source": "/:path*",
-        "headers": [
-          {
-            "key": "home",
-            "value": "true"
-          }
-        ]
-      }
-    ]
-  },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          "source": "/demos/:demo",
-          "destination": "https://www.:demo.flatlinediver.com/",
-          "basePath": false
-        },
-        {
-          "source": "/demos/:demo/:resources*",
-          "destination": "https://www.:demo.flatlinediver.com/:resources*",
-          "basePath": false
-        }
-      ]
-    }
-  },
+  reactStrictMode: true
 }
